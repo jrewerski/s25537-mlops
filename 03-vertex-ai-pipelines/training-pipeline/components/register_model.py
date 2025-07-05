20f8494c6f76ede391a4554739185a5ebaf3f172
jrewerski <thejansio3@gmail.com>
2025-07-05 19:59:10 +0200

    puffin

---
+++ 03-vertex-ai-pipelines/training-pipeline/components/register_model.py
@@ -0,0 +1,42 @@
+import kfp
+from kfp import dsl
+from kfp.dsl import (Artifact,
+                        Dataset,
+                        Input,
+                        InputPath,
+                        Output,
+                        OutputPath,
+                        component,
+                        Model
+                        )
+
+@component(
+    base_image="python:3.9",
+    packages_to_install=["pandas", "google-cloud-aiplatform==1.55.0", "gcsfs==2024.6.0", "fsspec", "pyarrow", "scikit-learn==1.5.0" ],
+)
+def register_model(
+    model: Input[Model],
+    project_id: str,
+    region: str,
+    model_display_name: str,
+):
+    """Rejestruje model w Vertex AI Model Registry."""
+    from google.cloud import aiplatform
+    import os
+
+    print(f"project_id : {project_id}")
+    print(f"region : {region}")
+    print(f"model : {model}")
+
+    aiplatform.init(project=project_id, location=region)
+    serving_container_image = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-5:latest"
+    model_path = '/'.join(model.uri.split('/')[:-1])
+    # Przesłanie i rejestracja modelu
+    registered_model = aiplatform.Model.upload(
+        display_name=model_display_name,
+        artifact_uri=model_path,
+        serving_container_image_uri=serving_container_image,
+        sync=True,
+        labels = {"model_type": "svc", "framework" : "scikit-learn"}
+    )
+    print(f"Zarejestrowano model: {registered_model.resource_name}")
---
+++ 03-vertex-ai-pipelines/training-pipeline/runner.py
@@ -0,0 +1,67 @@
+import kfp
+from kfp import dsl
+from kfp.dsl import (Artifact,
+                        Dataset,
+                        Input,
+                        InputPath,
+                        Model,
+                        Output,
+                        OutputPath,
+                        component,
+                        Metrics,
+                        pipeline)
+from kfp import compiler
+from google.cloud import aiplatform
+from typing import NamedTuple
+import logging
+
+from components.get_data import get_data
+from components.preprocess_data import preprocess_data
+from components.train_svc_model import train_svc_model
+from components.evaluate_svc_model import evaluate_svc_model
+from components.register_model import register_model
+
+
+
+# --- Definicja głównego potoku Vertex AI ---
+@pipeline(
+    name="penguin-svc-classification-with-registry",
+    description="Potok trenujący i rejestrujący model SVC.",
+    pipeline_root="gs://vertex-ai-bucket-s25537",
+)
+def penguin_pipeline(
+    gcs_data_path: str = "gs://data-s25537/penguins.csv",
+    project_id: str = "mlops-on-gcp-s25537",
+    region: str = "us-central1",
+    model_name: str = "puffin",
+    test_split_ratio: float = 0.3,
+    min_accuracy_threshold: float = 95.0,
+):
+    """Definiuje przepływ pracy w potoku z warunkową rejestracją."""
+    get_data_task = get_data(gcs_input_path=gcs_data_path)
+    
+    transform_data_task = preprocess_data(
+        input_data=get_data_task.outputs["input_data"],
+        test_split_ratio=test_split_ratio
+    )
+    
+    train_task = train_svc_model(
+        train_dataset=transform_data_task.outputs["train_dataset"]
+    )
+    
+    evaluate_task = evaluate_svc_model(
+        test_dataset=transform_data_task.outputs["test_dataset"],
+        model=train_task.outputs["model"],
+    )
+
+    # Warunek: zarejestruj model tylko, jeśli dokładność jest wystarczająco wysoka
+    with dsl.If(
+        evaluate_task.outputs["accuracy"] >= min_accuracy_threshold,
+        name="accuracy-check",
+    ):
+        register_model(
+            project_id=project_id,
+            region=region,
+            model_display_name=model_name,
+            model=train_task.outputs["model"],
+        )

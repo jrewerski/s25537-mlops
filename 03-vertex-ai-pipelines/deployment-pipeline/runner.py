9482e7c7b81d244d614c5ce80dfd023c78df0637
jrewerski <thejansio3@gmail.com>
2025-07-07 19:48:06 +0200

    fdgdgd

---
+++ 03-vertex-ai-pipelines/deployment-pipeline/runner.py
@@ -0,0 +1,53 @@
+import os
+import kfp
+from kfp import dsl
+from kfp.dsl import (Artifact,
+                        Dataset,
+                        Input,
+                        InputPath,
+                        Model,
+                        Output,
+                        OutputPath,
+                        component,
+                        Metrics,
+                        pipeline)
+from kfp import compiler
+from google.cloud import aiplatform
+from typing import NamedTuple
+from google_cloud_pipeline_components.v1.endpoint import EndpointCreateOp, ModelDeployOp
+
+
+
+@pipeline(
+    name="deployment-pipeline",
+    description="Potok tworzy endpoint i wdraża na nim podany model",
+    pipeline_root="gs://vertex-ai-bucket-s25537/deployment-pipeline",
+)
+def deployment_pipeline(
+    endpoint_name: str,
+    model_resource_name: str,
+    project_id: str = "mlops-on-gcp-s25537",
+    region: str = "us-central1",
+):
+    """Tworzy endpoint i wdraża na nim podany model."""
+
+    endpoint_create = EndpointCreateOp(
+        project = project_id,
+        location = region,
+        display_name = endpoint_name
+    )
+
+    model_deploy = ModelDeployOp(
+        model = model_resource_name,
+        endpoint = endpoint_create.outputs["endpoint"],
+        automatic_resources_min_replica_count=1,
+        automatic_resources_max_replica_count=1,
+    )
+
+
+if __name__ == '__main__':
+    print("Kompilacja potoku")
+    compiler.Compiler().compile(
+        pipeline_func=deployment_pipeline,
+        package_path="pipeline.json",
+    )
